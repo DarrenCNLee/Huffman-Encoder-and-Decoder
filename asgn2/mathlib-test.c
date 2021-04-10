552f168664c46f749d8c190c552e510b0a74db7f
Darren <danalee@ucsc.edu>
2021-04-10 16:49:58 -0700

    adding mathlib-test.c

---
+++ asgn2/mathlib-test.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "mathlib.c"
+
+#define OPTIONS "asctl"
+
+int main(int argc, char **argv) {
+    int opt = 0;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'a':
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf(
+                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcSin(i), asin(i), arcSin(i) - asin(i));
+            }
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf(
+                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcCos(i), acos(i), arcCos(i) - acos(i));
+            }
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf(
+                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcTan(i), atan(i), arcTan(i) - atan(i));
+            }
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, Log(i), log(i), Log(i) - log(i));
+            }
+            break;
+        case 's':
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf(
+                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcSin(i), asin(i), arcSin(i) - asin(i));
+            }
+            break;
+        case 'c':
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf(
+                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcCos(i), acos(i), arcCos(i) - acos(i));
+            }
+            break;
+        case 't':
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf(
+                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcTan(i), atan(i), arcTan(i) - atan(i));
+            }
+            break;
+        case 'l':
+            for (double i = -1.0; i <= 1.0; i += 0.1) {
+                printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, Log(i), log(i), Log(i) - log(i));
+            }
+            break;
+        }
+    }
+    return 0;
+}
