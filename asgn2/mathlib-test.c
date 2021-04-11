09be68b3bb8de47eaa7c17ef61eb64541fe157d2
Darren <danalee@ucsc.edu>
2021-04-10 19:13:43 -0700

    fixing epsilon value, adding Writeup and Makefile, fixing formatting of test

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -1,3 +1,11 @@
+// Darren Lee
+// CruzID: danalee
+// CSE 13S Spring 2021
+//
+// This program contains the code for the test harness
+// for the math library mathlib.c. It contains
+// tests for arcSin, arcCos, arcTan, and Log.
+
 #include "mathlib.c"
 
 #include <stdio.h>
@@ -7,46 +15,80 @@
 
 int main(int argc, char **argv) {
     int opt = 0;
+    int sin_test = 0, cos_test = 0, tan_test = 0,
+        log_test = 0; // test state is 1 if the test has been run; otherwise, test state is 0
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
-        case 'a':
-            for (double i = -1.0; i <= 1.0; i += 0.1) {
-                printf(
-                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcSin(i), asin(i), arcSin(i) - asin(i));
+        case 'a': // run all tests that have not been run yet if user types -a on command line
+            if (!sin_test) {
+                printf(" x           arcSin          Library       Difference\n");
+                for (double i = -1.0; i <= 1.0; i += 0.1) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcSin(i), asin(i),
+                        arcSin(i) - asin(i));
+                }
+                sin_test = 1; // arcSin test has been run
             }
-            for (double i = -1.0; i <= 1.0; i += 0.1) {
-                printf(
-                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcCos(i), acos(i), arcCos(i) - acos(i));
+            if (!cos_test) {
+                printf(" x           arcCos          Library       Difference\n");
+                for (double i = -1.0; i <= 1.0; i += 0.1) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcCos(i), acos(i),
+                        arcCos(i) - acos(i));
+                }
+                cos_test = 1; // arcCos test has been run
             }
-            for (double i = -1.0; i <= 1.0; i += 0.1) {
-                printf(
-                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcTan(i), atan(i), arcTan(i) - atan(i));
+            if (!tan_test) {
+                printf(" x           arcTan          Library       Difference\n");
+                for (double i = -1.0; i <= 1.0; i += 0.1) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcTan(i), atan(i),
+                        arcTan(i) - atan(i));
+                }
+                tan_test = 1; // arcTan test has been run
             }
-            for (double i = -1.0; i <= 1.0; i += 0.1) {
-                printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, Log(i), log(i), Log(i) - log(i));
-            }
-            break;
-        case 's':
-            for (double i = -1.0; i <= 1.0; i += 0.1) {
-                printf(
-                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcSin(i), asin(i), arcSin(i) - asin(i));
-            }
-            break;
-        case 'c':
-            for (double i = -1.0; i <= 1.0; i += 0.1) {
-                printf(
-                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcCos(i), acos(i), arcCos(i) - acos(i));
-            }
-            break;
-        case 't':
-            for (double i = -1.0; i <= 1.0; i += 0.1) {
-                printf(
-                    "%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcTan(i), atan(i), arcTan(i) - atan(i));
+            if (!log_test) {
+                printf(" x           Log             Library       Difference\n");
+                for (double i = 1.0; i <= 10.0; i += 0.5) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, Log(i), log(i), Log(i) - log(i));
+                }
+                log_test = 1; // Log test has been run
             }
             break;
-        case 'l':
-            for (double i = 1.0; i <= 10.0; i += 0.5) {
-                printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, Log(i), log(i), Log(i) - log(i));
+        case 's': // run arcSin test if it has not been run yet if user types -s on command line
+            if (!sin_test) {
+                printf(" x           arcSin          Library       Difference\n");
+                for (double i = -1.0; i <= 1.0; i += 0.1) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcSin(i), asin(i),
+                        arcSin(i) - asin(i));
+                }
+                sin_test = 1; // arcSin test has been run
+            }
+            break;
+        case 'c': // run arcCos test if it has not been run yet if user types -c on command line
+            if (!cos_test) {
+                printf(" x           arcCos          Library       Difference\n");
+                for (double i = -1.0; i <= 1.0; i += 0.1) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcCos(i), acos(i),
+                        arcCos(i) - acos(i));
+                }
+                cos_test = 1; // arcCos test has been run
+            }
+            break;
+        case 't': // run arcTan test if it has not been run yet if user types -t on command line
+            if (!tan_test) {
+                printf(" x           arcTan          Library       Difference\n");
+                for (double i = -1.0; i <= 1.0; i += 0.1) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, arcTan(i), atan(i),
+                        arcTan(i) - atan(i));
+                }
+                tan_test = 1; // arcTan test has been run
+            }
+            break;
+        case 'l': // run Log test if it has not been run yet if user types -l on the command line
+            if (!log_test) {
+                printf(" x           Log             Library       Difference\n");
+                for (double i = 1.0; i <= 10.0; i += 0.5) {
+                    printf("%7.4lf%16.8lf%16.8lf%16.10lf\n", i, Log(i), log(i), Log(i) - log(i));
+                }
+                log_test = 1; // Log test has been run
             }
             break;
         }
--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -1,12 +1,24 @@
+// Darren Lee
+// CruzID: danalee
+// CSE 13S Spring 2021
+//
+// This program defines the functions for the math library.
+// The included functions are Abs, Exp, Sqrt, arcSin, arcCos,
+// and Log.
+
 #include <assert.h>
 #include <math.h>
 
-#define EPSILON 1e-12
+#define EPSILON 1e-10
 
+// Returns the absolute value of x
+// x: double of which to take the absolute value
 double Abs(double x) {
     return (x < 0) ? -x : x;
 }
 
+// Returns e raised to x
+// x: the power to raise x to
 double Exp(double x) { // code from Piazza
     double term = 1, sum = 1;
     for (int k = 1; Abs(term) > EPSILON; k += 1) {
@@ -16,6 +28,8 @@ double Exp(double x) { // code from Piazza
     return sum;
 }
 
+// Returns the square root of x
+// x: the value of which to take the square root
 double Sqrt(double x) { // code from Piazza
     double y = 1.0;
     assert(x >= 0);
@@ -25,6 +39,8 @@ double Sqrt(double x) { // code from Piazza
     return y;
 }
 
+// Returns the arcsin of x
+// x: the value of which to take the arcsin
 double arcSin(double x) {
     double m, l = -M_PI / 2, h = M_PI / 2;
     do {
@@ -38,14 +54,20 @@ double arcSin(double x) {
     return m;
 }
 
+// Returns the arccos of x
+// x: value of which to take the arccos
 double arcCos(double x) {
     return M_PI / 2 - arcSin(x);
 }
 
+// Returns the arctan of x
+// x: value of which to take the arctan
 double arcTan(double x) {
     return arcSin(x / Sqrt(x * x + 1));
 }
 
+// Returns the log base n of x
+// x: value of which to take the natural log
 double Log(double x) {
     double k = 1.0;
     assert(x > 0);
